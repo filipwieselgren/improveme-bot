578f87054a7354ffa17847a0f3e62a3e2ab09c73
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-12-21 14:43:30 +0100

    first commit

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,15 @@
+import logo from "../../assets/ImproveMe.png";
+
+const Main = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <img src={logo} alt="Logo image" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
